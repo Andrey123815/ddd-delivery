bacee51fe20ee2fc376fa506bd327410bdcfb2f4
Andrey Diakonov <ivredomon@gmail.com>
2026-03-26 23:42:48 +0300

    feat: add local go generate insfructructure

---
+++ internal/generated/servers/generate.go
@@ -0,0 +1,3 @@
+package servers
+
+//go:generate oapi-codegen -config ../../../configs/server.cfg.yaml https://gitlab.com/microarch-ru/ddd-in-practice/system-design/-/raw/main/services/delivery/contracts/openapi.yml
